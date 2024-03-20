caf121e9d9109ab54b680a98a735fb066c351c20
speedyibbi <ibbi2k2.khan@gmail.com>
2024-03-20 14:25:14 +0500

    create database class

--- SocietySyncLibrary/Database.cs
+++ SocietySyncLibrary/Database.cs
@@ -1,6 +1,44 @@
-﻿namespace SocietySyncLibrary;
+﻿using Microsoft.Data.SqlClient;
 
-public class Database
+namespace SocietySyncLibrary;
+
+public sealed class Database
 {
+    private static Database? _instance;
+    private static readonly object _syncRoot = new object();
+    private readonly SqlConnection _connection;
 
+    public static Database Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                lock (_syncRoot)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Database();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public SqlConnection Connection
+    {
+        get { return _connection; }
+    }
+
+    private Database()
+    {
+        _connection = new SqlConnection("Data Source=LOPTAP\\SQLEXPRESS;Initial Catalog=SocietySyncDB;Integrated Security=True;Trust Server Certificate=True");
+        _connection.Open();
+    }
+
+    ~Database()
+    {
+        _connection.Close();
+    }
 }
